2cd5384345c858c552a076c2b98c1d98dc1a1266
chunwei <tanchunwei@gmail.com>
2020-04-13 11:32:13 +0800

    Add test product not found, only display failed message with hardcoded barcode

--- src/main/kotlin/PointOfSales.kt
+++ src/main/kotlin/PointOfSales.kt
@@ -2,8 +2,10 @@ class PointOfSales(val display: Display) {
     fun onBarcode(barcode: String) {
         if(barcode == "12345")
             display.setText("$7.99")
-        else
+        else if(barcode == "67890")
             display.setText("$10.99")
+        else
+            display.setText("This product does not exist 556677")
     }
 
 }
--- src/test/kotlin/POSTest.kt
+++ src/test/kotlin/POSTest.kt
@@ -1,3 +1,4 @@
+import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -10,11 +11,19 @@ class POSTest {
         "67890,$10.99"
     ])
     fun productFound(barcode : String, expected : String){
-
         val display = Display()
         val pos = PointOfSales(display)
 
         pos.onBarcode(barcode)
         assertEquals(expected, display.getText())
+    }
+
+    @Test
+    fun productNotFound(){
+        val display = Display()
+        val pos = PointOfSales(display)
+
+        pos.onBarcode("556677")
+        assertEquals("This product does not exist 556677", display.getText())
     }
 }
