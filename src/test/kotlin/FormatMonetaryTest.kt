7196a877ab425c40401cb711ceaac71bc38cda91
chunwei <tanchunwei@gmail.com>
2020-04-14 16:28:01 +0800

    Add more test scenarios for FormatMonetaryTest to handle thousands seperator change parameterized test delimeter

--- src/test/kotlin/FormatMonetaryTest.kt
+++ src/test/kotlin/FormatMonetaryTest.kt
@@ -5,17 +5,18 @@ import org.junit.jupiter.params.provider.CsvSource
 class FormatMonetaryTest {
     @ParameterizedTest(name = "Format {0} to {1}")
     @CsvSource(value = [
-        "799, $7.99",
-        "1299, $12.99",
-        "1000, $10.00",
-        "0, $0.00",
-        "3, $0.03"
-    ])
+        "799| $7.99",
+        "1299| $12.99",
+        "1000| $10.00",
+        "0| $0.00",
+        "3| $0.03",
+        "123456| $1,234.56"
+    ], delimiter = '|')
     fun formatMonetary(price : Int, expectedFormat : String){
         assertEquals(expectedFormat, formatMonetary(price) )
     }
 
     private fun formatMonetary(price: Int): String {
-        return "$${String.format("%.2f",price.toFloat() / 100)}"
+        return "$${String.format("%,.2f",price.toFloat() / 100)}"
     }
 }
