e8a611f118e14a78946535c659a5f250e9da2b88
chunwei <tanchunwei@gmail.com>
2020-04-14 17:58:36 +0800

    Change it to extension method

--- src/test/kotlin/ComputeCartTotalTest.kt
+++ src/test/kotlin/ComputeCartTotalTest.kt
@@ -1,4 +1,3 @@
-import controller.PointOfSales
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -6,23 +5,27 @@ class ComputeCartTotalTest {
     @Test
     fun computeZeroItem(){
         val listOfInt = mutableListOf<Int>()
-        assertEquals(0, computeCartTotal(listOfInt))
+        assertEquals(0, listOfInt.computeCartTotal())
     }
 
     @Test
     fun computeOneItem(){
         val listOfInt = mutableListOf(4)
-        assertEquals(4, computeCartTotal(listOfInt))
+        assertEquals(4, listOfInt.computeCartTotal())
     }
 
     @Test
     fun computeSeveralItem(){
         val listOfInt = mutableListOf(4, 3, 1, 10)
-        assertEquals(18, computeCartTotal(listOfInt))
+        assertEquals(18, listOfInt.computeCartTotal())
     }
 
-    private fun computeCartTotal(listOfInt: MutableList<Int>): Int {
-        return PointOfSales.computeCartTotal(listOfInt)
+    private fun MutableList<Int>.computeCartTotal(): Int {
+        var total = 0
+        for (price in this) {
+            total += price
+        }
+        return total
     }
 
 }
