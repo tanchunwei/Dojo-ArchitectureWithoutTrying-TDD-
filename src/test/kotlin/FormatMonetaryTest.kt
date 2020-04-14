8f3c62aa0ad58c00ef13fdd3bb7d084df077df14
chunwei <tanchunwei@gmail.com>
2020-04-14 16:35:33 +0800

    Add more test scenarios for FormatMonetaryTest to handle larger thousands value. Apparently, using float / 100 giving different result hence fail the test added. Changing to use big decimal

--- src/test/kotlin/FormatMonetaryTest.kt
+++ src/test/kotlin/FormatMonetaryTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.math.BigDecimal
 
 class FormatMonetaryTest {
     @ParameterizedTest(name = "Format {0} to {1}")
@@ -10,13 +11,14 @@ class FormatMonetaryTest {
         "1000| $10.00",
         "0| $0.00",
         "3| $0.03",
-        "123456| $1,234.56"
+        "123456| $1,234.56",
+        "122033981| $1,220,339.81"
     ], delimiter = '|')
     fun formatMonetary(price : Int, expectedFormat : String){
         assertEquals(expectedFormat, formatMonetary(price) )
     }
 
     private fun formatMonetary(price: Int): String {
-        return "$${String.format("%,.2f",price.toFloat() / 100)}"
+        return "$${String.format("%,.2f",price.toBigDecimal().divide(BigDecimal(100)))}"
     }
 }
