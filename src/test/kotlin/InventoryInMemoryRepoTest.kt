edd5fa1c39d413cb159c6278d6bf863d924f393c
chunwei <tanchunwei@gmail.com>
2020-04-15 15:57:08 +0800

    Refactor to have abstract InventoryTest hence any implement will inherit test the same set of test case

--- src/test/kotlin/InventoryInMemoryRepoTest.kt
+++ src/test/kotlin/InventoryInMemoryRepoTest.kt
@@ -1,31 +1,14 @@
 import model.Price
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import repository.interfaces.IInventory
 import repository.InventoryInMemoryRepo
 
-class InventoryInMemoryRepoTest {
-    @Test
-    fun priceFound(){
-        val priceInProduct = Price(1250)
+class InventoryInMemoryRepoTest : InventoryTest() {
 
-        val actualPrice = inventoryWith("12345", priceInProduct).getInventory("12345")
-
-        assertEquals(priceInProduct, actualPrice)
-    }
-
-    @Test
-    fun priceNotFound(){
-        val actualPrice = inventoryWithout("12345").getInventory("12345")
-
-        assertEquals(null, actualPrice)
-    }
-
-    private fun inventoryWith(barcode: String, price: Price): IInventory {
+    override fun inventoryWith(barcode: String, price: Price): IInventory {
         return InventoryInMemoryRepo(mapOf(barcode to price))
     }
 
-    private fun inventoryWithout(barcode : String): IInventory {
+    override fun inventoryWithout(barcode : String): IInventory {
         return InventoryInMemoryRepo(mapOf("any barcode except $barcode" to Price(0)))
     }
 }
