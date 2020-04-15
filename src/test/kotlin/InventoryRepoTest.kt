74318dab99f839eba84f932e224ab314c0c832ae
chunwei <tanchunwei@gmail.com>
2020-04-15 15:37:33 +0800

    Refactor to test to more self documentary naming

--- src/test/kotlin/InventoryRepoTest.kt
+++ src/test/kotlin/InventoryRepoTest.kt
@@ -8,19 +8,24 @@ class InventoryRepoTest {
     @Test
     fun priceFound(){
         val priceInProduct = Price(1250)
-        val inventoryRepo = InventoryRepo(mapOf("12345" to priceInProduct))
 
-        val actualPrice = inventoryRepo.getInventory("12345")
+        val actualPrice = inventoryWith("12345", priceInProduct).getInventory("12345")
 
         assertEquals(priceInProduct, actualPrice)
     }
 
     @Test
     fun priceNotFound(){
-        val inventoryRepo = InventoryRepo(mapOf())
-
-        val actualPrice = inventoryRepo.getInventory("67890")
+        val actualPrice = inventoryWithout("12345").getInventory("12345")
 
         assertEquals(null, actualPrice)
+    }
+
+    private fun inventoryWith(barcode: String, price: Price): InventoryRepo {
+        return InventoryRepo(mapOf(barcode to price))
+    }
+
+    private fun inventoryWithout(barcode : String): InventoryRepo {
+        return InventoryRepo(mapOf("any barcode except $barcode" to Price(0)))
     }
 }
