ddd6ee891414d5fc71d069aec357a0fa97234498
chunwei <tanchunwei@gmail.com>
2020-04-20 11:58:01 +0800

    Add test and implement web display

---
+++ src/main/kotlin/view/WebDisplay.kt
@@ -0,0 +1,24 @@
+package com.pos.view
+
+import model.Price
+import view.interfaces.IDisplay
+
+class WebDisplay : IDisplay {
+    private var display: String = ""
+
+    override fun displayPrice(price: Price) {
+        display = price.toString()
+    }
+
+    override fun displayProductNotFound(barcode: String) {
+        display = "Product not found [$barcode]"
+    }
+
+    override fun displayEmptyBarcode() {
+        display = "Barcode is empty. Please try to rescan"
+    }
+
+    fun toJson(): String {
+        return "{'display':'$display'}"
+    }
+}
---
+++ src/test/kotlin/WebDisplayTest.kt
@@ -0,0 +1,33 @@
+import com.pos.view.WebDisplay
+import model.Price
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WebDisplayTest{
+    @Test
+    fun displayPrice(){
+        val display = WebDisplay()
+
+        display.displayPrice(Price(1250))
+
+        assertEquals("{'display':'$12.50'}", display.toJson())
+    }
+
+    @Test
+    fun displayProductNotFound(){
+        val display = WebDisplay()
+
+        display.displayProductNotFound("barcode")
+
+        assertEquals("{'display':'Product not found [barcode]'}", display.toJson())
+    }
+
+    @Test
+    fun displayEmptyBarcode(){
+        val display = WebDisplay()
+
+        display.displayEmptyBarcode()
+
+        assertEquals("{'display':'Barcode is empty. Please try to rescan'}", display.toJson())
+    }
+}
