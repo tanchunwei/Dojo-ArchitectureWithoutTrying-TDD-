abdadecc1eb7f6578df8432ece325f934dfdf2f0
chunwei <tanchunwei@gmail.com>
2020-04-21 11:05:34 +0800

    prepare docker web to scale up however it could only scale up with pre-gen port but not 8090

--- docker.sh
+++ docker.sh
@@ -1,2 +1,11 @@
+#example of how to build image from Dockerfile
+#sudo docker build -f Dockerfile -t pos-1.0 .
+
+#example to remove untagged images (happen when build image with same name, the old image will be untagged)
+#sudo docker rmi $(sudo docker images -f"dangling=true" -q)
+
+#example of how to create container from images
 #sudo docker container create --name pos-s1 -p 8090:8080 pos-1.0
+
 sudo docker-compose up -d
+sudo docker-compose --scale web=4
